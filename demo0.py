bc6ef004277ae9fd07b4d6ae3d5631707ff6a81e
BodanChen <18377475@buaa.edu.cn>
2022-04-11 15:13:21 +0800

    showdemo0

--- demo0.py
+++ demo0.py
@@ -2,7 +2,7 @@
 author: Bodan Chen
 Date: 2022-04-09 16:46:30
 LastEditors: Bodan Chen
-LastEditTime: 2022-04-09 17:01:53
+LastEditTime: 2022-04-10 21:16:30
 Email: 18377475@buaa.edu.cn
 '''
 ##  基础函数库
@@ -16,3 +16,51 @@ import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 
 ##Demo演示LogisticRegression分类
+
+## 构造数据集
+x_fearures = np.array([[-1, -2], [-2, -1], [-3, -2], [1, 3], [2, 1], [3, 2]])
+y_label = np.array([0,0,0,1,1,1])
+
+## 调用逻辑回归模型
+lr_clf = LogisticRegression()
+
+## 用逻辑回归模型拟合构造的数据集
+lr_clf = lr_clf.fit(x_fearures, y_label) #其拟合方程为 y=w0+w1*x1+w2*x2
+w=lr_clf.coef_
+w0=lr_clf.intercept_
+
+print(w0+np.dot(w,x_fearures.T))
+## 查看其对应模型的w
+print('the weight of Logistic Regression:',lr_clf.coef_)
+
+## 查看其对应模型的w0
+print('the intercept(w0) of Logistic Regression:',lr_clf.intercept_)
+
+## 可视化构造的数据样本点
+plt.figure()
+
+## new point 1
+x_fearures_new1 = np.array([[0, -1]])
+plt.scatter(x_fearures_new1[:,0],x_fearures_new1[:,1], s=50, cmap='viridis')
+plt.annotate(s='New point 1',xy=(0,-1),xytext=(-2,0),color='blue',arrowprops=dict(arrowstyle='-|>',connectionstyle='arc3',color='red'))
+
+## new point 2
+x_fearures_new2 = np.array([[1, 2]])
+plt.scatter(x_fearures_new2[:,0],x_fearures_new2[:,1], s=50, cmap='viridis')
+plt.annotate(s='New point 2',xy=(1,2),xytext=(-1.5,2.5),color='red',arrowprops=dict(arrowstyle='-|>',connectionstyle='arc3',color='red'))
+
+## 训练样本
+plt.scatter(x_fearures[:,0],x_fearures[:,1], c=y_label, s=50, cmap='viridis')
+plt.title('Dataset')
+
+nx, ny = 200, 100
+x_min, x_max = plt.xlim()
+print(x_min,x_max)
+y_min, y_max = plt.ylim()
+x_grid, y_grid = np.meshgrid(np.linspace(x_min, x_max, nx),np.linspace(y_min, y_max, ny))
+
+z_proba = lr_clf.predict_proba(np.c_[x_grid.ravel(), y_grid.ravel()])
+z_proba = z_proba[:, 1].reshape(x_grid.shape)
+plt.contour(x_grid, y_grid, z_proba, [0.5], linewidths=2., colors='blue')
+
+plt.show()
---
+++ demo1_iris.py
@@ -0,0 +1,14 @@
+'''
+author: Bodan Chen
+Date: 2022-04-10 21:18:00
+LastEditors: Bodan Chen
+LastEditTime: 2022-04-10 21:18:00
+Email: 18377475@buaa.edu.cn
+'''
+##  基础函数库
+import numpy as np 
+import pandas as pd
+
+## 绘图函数库
+import matplotlib.pyplot as plt
+import seaborn as sns
