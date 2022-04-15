53d3a3342057f31e24c59d9384d36e21faf12098
BodanChen <18377475@buaa.edu.cn>
2022-04-15 16:58:42 +0800

    add demo2-1.py

---
+++ demo2_1.py
@@ -0,0 +1,22 @@
+'''
+author: Bodan Chen
+Date: 2022-04-14 17:07:06
+LastEditors: Bodan Chen
+LastEditTime: 2022-04-15 16:58:10
+Email: 18377475@buaa.edu.cn
+'''
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+# 加载莺尾花数据集
+from sklearn import datasets
+# 导入高斯朴素贝叶斯分类器
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+
+X, y = datasets.load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# 使用高斯朴素贝叶斯进行计算
+clf = GaussianNB(var_smoothing=1e-8)
+clf.fit(X_train, y_train)
