67d0172f241fa04f6a7b88c71b929fee58ffc882
BodanChen <18377475@buaa.edu.cn>
2022-04-15 19:46:16 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+author: Bodan Chen
+Date: 2022-04-15 19:45:39
+LastEditors: Bodan Chen
+LastEditTime: 2022-04-15 19:45:39
+Email: 18377475@buaa.edu.cn
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import warnings
+warnings.filterwarnings('ignore')
--- test.py
+++ test.py
@@ -2,23 +2,29 @@
 author: Bodan Chen
 Date: 2022-02-16 16:30:29
 LastEditors: Bodan Chen
-LastEditTime: 2022-02-18 01:59:53
+LastEditTime: 2022-04-15 19:45:09
 Email: 18377475@buaa.edu.cn
 '''
-#Code: https://github.com/MorvanZhou/PyTorch-Tutorial
-import torch
-import numpy as np
+# #Code: https://github.com/MorvanZhou/PyTorch-Tutorial
+# import torch
+# import numpy as np
+# 导入数据读取模块
+import pandas as pd
 
 
-# details about math operation in torch can be found in: http://pytorch.org/docs/torch.html#math-operations
-np_data=np.arange(6)
-torch_data = torch.from_numpy(np_data)
-tensor2array=torch_data.numpy()
-print(
-    '\nnum array:',np_data,
-    '\ntorch tensor:',torch_data,
-    '\ntensor to array:',tensor2array,
-)
+# # details about math operation in torch can be found in: http://pytorch.org/docs/torch.html#math-operations
+# np_data=np.arange(6)
+# torch_data = torch.from_numpy(np_data)
+# tensor2array=torch_data.numpy()
+# print(
+#     '\nnum array:',np_data,
+#     '\ntorch tensor:',torch_data,
+#     '\ntensor to array:',tensor2array,
+# )
 
-# np.abs(data)
-# torch.abs(data)
+# # np.abs(data)
+# # torch.abs(data)
+
+print("****************下载文件中。。。")
+#!wget http://tianchi-media.oss-cn-beijing.aliyuncs.com/dragonball/DL/other/data/Emotion_Recognition_File.zip
+print("****************下载完成。。。")
