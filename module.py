00a75bbfb35d76bd7820a67c0a926d6fb18767ec
BodanChen <18377475@buaa.edu.cn>
2022-04-17 16:40:30 +0800

    add modual.py

---
+++ module.py
@@ -0,0 +1,68 @@
+'''
+author: Bodan Chen
+Date: 2022-04-17 16:35:31
+LastEditors: Bodan Chen
+LastEditTime: 2022-04-17 16:40:13
+Email: 18377475@buaa.edu.cn
+'''
+import pandas as pd
+import numpy as np
+import warnings
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+"""
+sns 相关设置
+@return:
+"""
+# 声明使用 Seaborn 样式
+sns.set()
+# 有五种seaborn的绘图风格，它们分别是：darkgrid, whitegrid, dark, white, ticks。默认的主题是darkgrid。
+sns.set_style("whitegrid")
+# 有四个预置的环境，按大小从小到大排列分别为：paper, notebook, talk, poster。其中，notebook是默认的。
+sns.set_context('talk')
+# 中文字体设置-黑体
+plt.rcParams['font.sans-serif'] = ['SimHei']
+# 解决保存图像是负号'-'显示为方块的问题
+plt.rcParams['axes.unicode_minus'] = False
+# 解决Seaborn中文显示问题并调整字体大小
+sns.set(font='SimHei')
+
+# reduce_mem_usage 函数通过调整数据类型，帮助我们减少数据在内存中占用的空间
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() 
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() 
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    return df
